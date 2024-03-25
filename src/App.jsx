c3de27752299a40b0f5f39ff098f1b5ea9eed26e
buhtig-sudo <reservs@inbox.ru>
2024-03-25 15:56:30 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react';
+const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
+  
 const Part = (props) => {
   return (
     <>
@@ -20,13 +28,7 @@ const Content = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
 
   return <>
 
