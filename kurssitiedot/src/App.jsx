0430c85c008131b6cae171f3a97bc97775fbdfbd
buhtig-sudo <reservs@inbox.ru>
2024-04-15 05:09:55 +0300

    sex commit

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
+const History = ({ all }) => {
 
+  if (all.length == 0) {
+    return (
+      <h4>Click button please!</h4>
+    )
+  }
+  return <h4>{all.join("")}</h4>
+
+}
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
-
+  const [total, setTotal] = useState(0);
   const handleLeftClick = () => {
-    setAll(allClicks.concat("L"))
-    setClicks({ ...clicks, left: clicks.left + 1 })
+    setAll(allClicks.concat('L'));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
   }
   const handleRightClick = () => {
-    setAll(allClicks.concat("R"))
-    setClicks({ ...clicks, right: clicks.right + 1 })
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(updatedRight + left);
   }
 
-
+  console.log(typeof total)
   return (
     <>
       <div>
-        <button onClick={handleLeftClick}>click</button>{clicks.left} --- {clicks.right}<button onClick={handleRightClick}>click</button>
+        <button onClick={handleLeftClick}>click</button>{left} --- {right}<button onClick={handleRightClick}>click</button>
       </div>
       <div>
-        {allClicks.join('')}
+        <History all={allClicks} />
+        <h4>Total {total}</h4>
       </div>
     </>
   )
