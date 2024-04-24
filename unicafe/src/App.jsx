e9bb9ca85dd36db6f45880b63587bda38eed28da
buhtig-sudo <reservs@inbox.ru>
2024-04-24 18:48:15 +0300

    five commit

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -10,13 +10,24 @@ export const Button = (props) => {
 }
 function App() {
   const [gcount, setCountGood] = useState(0);
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const all = gcount + neutral + bad;
   const reset = () => {
     setCountGood(0);
     setNeutral(0);
     setBad(0);
   }
+  
+  let average = (gcount - bad)/all;
+  let positive = (gcount /all)*100;
+  if (Number.isNaN(average) ) {
+    average = 0;
+  }
+  if (Number.isNaN(positive)) {
+    positive = 0;
+  }
+  
   return (
     <>
 
@@ -27,9 +38,13 @@ function App() {
       <Button text="bad" handler={() => setBad(bad + 1)} />
       <Button className={cl.creset} text="reset" handler={reset} />
 
+      <h4>Statistics</h4>
       <t> good: {gcount}</t>
       <t> neutral: {neutral}</t>
       <t> bad: {bad}</t>
+      <t> all: {all}</t>
+      <t>average: {average}</t>
+      <t>positive: {positive} %</t>
     </>
   )
 }
